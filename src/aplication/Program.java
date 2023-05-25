78e8aaedfa100111411af900ff5c80e45ad2ba0a
daviPC <kaliflamel@gmail.com>
2023-05-25 13:25:27 -0300

    fist class: Position

---
+++ src/aplication/Program.java
@@ -0,0 +1,13 @@
+package aplication;
+import boardgame.Position;
+public class Program {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello word");
+		Position pos = new Position(2,2);
+		
+		System.out.println(pos);
+	}
+
+}

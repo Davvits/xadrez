d462793dbec67367c4cdc9fd546609b465c433c4
daviPC <kaliflamel@gmail.com>
2023-05-27 13:47:23 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -50,4 +50,13 @@ public class Board {
 		if(!positionExists(position))throw new BoardException("Position not on the board");
 		return piece(position) != null;
 	}
+	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position))throw new BoardException("Position not on the board");
+		if(piece(position) == null) return null;
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 }

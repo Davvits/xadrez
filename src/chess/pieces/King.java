64a5f6829ed3db9d5ee4886ea36a87cb3d5e228e
davi R <kaliflamel@gmail.com>
2023-06-01 14:19:33 -0300

    Implementing possible moves of king

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
-package chess.pieces;
+ package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,10 +16,53 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
-
+	
+	private boolean canMove(Position position) {
+		
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		
+		return p == null || p.getColor() != getColor();
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0,0);
+		
+		
+		//Below and Right
+		for(int i = 0 ; i < 2; i++) {
+			for(int j = 0;j < 2; j++) {
+				p.setValues(position.getRow() + i, position.getColumn() + j);
+				if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+				
+			}
+		}
+		
+		//Above and Right
+		for(int i = 0 ; i < 2; i++) {
+			for(int j = 0;j<2; j++) {
+				p.setValues(position.getRow() - i, position.getColumn() + j);
+				if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+			}
+		}
+		
+		//Below and Left
+		for(int i = 0 ; i < 2; i++) {
+			for(int j = 0;j<2; j++) {
+				p.setValues(position.getRow() + i, position.getColumn() - j);
+				if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+			}
+		}
+		
+		//Above and Left
+		for(int i = 0 ; i < 2; i++) {
+			for(int j = 0;j<2; j++) {
+				p.setValues(position.getRow() - i, position.getColumn() - j);
+				if(getBoard().positionExists(p) && canMove(p)) mat[p.getRow()][p.getColumn()] = true;
+			}
+		}
 		return mat;
 	}
 
